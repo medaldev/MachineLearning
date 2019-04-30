81305499dda08023fc93d95fe109d8d27cce8cf6
medaldev <webformedal@gmail.com>
2019-04-30 23:28:02 +0300

    Листинг 2.4. Создание тензоров.

--- books/Nisнant_shuкla/examples/listing2-4.py
+++ books/Nisнant_shuкla/examples/listing2-4.py
@@ -1,23 +1,18 @@
 import tensorflow as tf
 import numpy as np
 
-m1 = [[1.0, 2.0],
-      [3.0, 4.0]]
+m1 = tf.constant([[1., 2.]])
 
-m2 = np.array([[1.0, 2.0],
-               [3.0, 4.0]], dtype=np.float32)
+m2 = tf.constant([[1.],
+                  [2.]])
 
-m3 = tf.constant([[1.0, 2.0],
-                  [3.0, 4.0]])
+m3 = tf.constant([[[1, 2],
+                   [1, 2],
+                   [1, 2]],
+                  [[7, 8],
+                   [9, 10],
+                   [11, 12]]])
 
-print(type(m1))
-print(type(m2))
-print(type(m3))
-
-t1 = tf.convert_to_tensor(m1, dtype=tf.float32)
-t2 = tf.convert_to_tensor(m2, dtype=tf.float32)
-t3 = tf.convert_to_tensor(m3, dtype=tf.float32)
-
-print(type(t1))
-print(type(t2))
-print(type(t3))
+print(m1)
+print(m2)
+print(m3)
